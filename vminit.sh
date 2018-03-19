1c86d7c6e5fbc1b0baa2432d8eb2df3d0122fe60
devops2k18 <34697016+devops2k18@users.noreply.github.com>
2018-03-19 09:10:24 +0530

    Update vminit.sh

--- vminit.sh
+++ vminit.sh
@@ -35,7 +35,7 @@ SELINUX() {
 PACK() {
 
 	Print SL "=>> Installing base Packages.. " B
-	yum install wget zip unzip gzip vim net-tools facter -y &>/dev/null
+	yum install wget zip unzip gzip vim net-tools facter bzip2 -y &>/dev/null
 	Print NL Success G
 }
 
